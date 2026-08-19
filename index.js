097d1e62de1485b84f1fbbe328a68b8dc68cd6af
camaraip55555 <camaraip55555@gmail.com>
2026-08-19 04:37:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ app.get('/api', (req, res) => {
     if (username === 'usuario1' && password === '1234') {
         const targetUrl = 'http://89.187.188.214:2086/YtLcIpxZzU/7816778667/26046?h=47fbda50df5fb1c8f687d4b68e78569d7fbecf91&z=62.36.126.17&i=463846&token=SkNbBRIMRF5GWl0AAAFaDAZUA1YBAA4AVVRbDFJSAlUJBFcAVAJVUFYbFBdAFktdA1tsXQBHDQBUB1YOThZBRFQTbA0FRlxGVgwPDQZaChpOFEddCQAVCFMAWg9TBAIHCFMfRhENAkZeGwkFA1MBAVAUHxYBHUNXEFkDVD1QUUFYAlZGWwoTCAgVGlpdPV1dD1ddUEZfFQJAG0BREUQWDV8UXwhNRgQNEEtZQVZAAxpQAAEERkkVUQ1CDEwQTRYNEyRgRk1GAxwQXFZGWg1XGlgUR0dGSRVbEWgQXRFARlJQDFYWQ15EVEYVGlhSGmZbDVhdUQcRXl0MREACQAUWGxMMXAoIEAkWO0lRURFYGwpaDwsMXFIVTw==';
         
-        // El servidor actúa como puente y transmite el video directamente a VLC
-        http.get(targetUrl, (proxyRes) => {
+        // Configuramos opciones para hacernos pasar por VLC
+        const options = {
+            headers: {
+                'User-Agent': 'VLC/3.0.18 LibVLC/3.0.18',
+                'Accept': '*/*'
+            }
+        };
+
+        http.get(targetUrl, options, (proxyRes) => {
             res.writeHead(proxyRes.statusCode, proxyRes.headers);
             proxyRes.pipe(res);
         }).on('error', (err) => {
